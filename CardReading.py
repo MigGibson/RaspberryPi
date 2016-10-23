a0155296ed7cfb859a8bf4aa27700729b47558ec
MigGibson <miguel.gibson135@gmail.com>
2016-10-23 03:10:48 +0200

    Working - Add CardID to Card DB

--- CardReading.py
+++ CardReading.py
@@ -98,7 +98,7 @@ while continue_reading:
                 if registration:
                     f = opener.open("http://192.168.0.19:9876/Service1.svc/RegisterParticipant/" + cardID)
                 else:
-                    f = opener.open("http://192.168.0.19:9876/Service1.svc/RecordTime/" + cardID + "/" + time.strftime("%d/%m/%Y"))
+                    f = opener.open("http://192.168.0.19:9876/Service1.svc/RecordTime/" + cardID + "/" + eventID)
                 #####################
                 #Do something with result:
                 response = f.read()
